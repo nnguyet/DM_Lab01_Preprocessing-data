e70447b7917f1cb6d012accb08cf8f57e15d946b
Nguyet <nguyetlms@gmail.com>
2021-11-17 19:11:48 +0700

    Add files

---
+++ dm.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+
+file = 'house-prices.csv'       # Gán cứng để tiết kiệm thời gian test code, xóa sau khi hoàn thành
+#file = sys.argv[1]      # Tham số dòng lệnh thứ 2 là tên file csv
+
+df = pd.read_csv(file)      # Đọc dữ liệu file csv
