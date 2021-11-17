fa7196a9fa7ff6212e7cf0ef2fe6bbf72c8a9600
Nguyet <nguyetlms@gmail.com>
2021-11-17 19:23:56 +0700

    Lay ten thuoc tinh

--- dm.py
+++ dm.py
@@ -5,3 +5,5 @@ file = 'house-prices.csv'       # Gán cứng để tiết kiệm thời g
 #file = sys.argv[1]      # Tham số dòng lệnh thứ 2 là tên file csv
 
 df = pd.read_csv(file)      # Đọc dữ liệu file csv
+title = list(df.columns)    # Lấy tên các thuộc tính
+print(title)
